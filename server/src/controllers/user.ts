6fb95ce6ef9dd1283ec6a95ca12c479724be9999
木杉 <zhbnh9988@163.com>
2018-05-16 21:42:25 +0800

    add job filed

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -52,10 +52,10 @@ export class UserController {
 
   static async updateOne(ctx: Context) {
     const user: IUser = ctx.state.user
-    const { nickname } = ctx.request.body
+    const { nickname, job } = ctx.request.body
 
     try {
-      const res = await UserService.updateOne(user, { nickname } as IUser)
+      const res = await UserService.updateOne(user, { nickname, job } as IUser)
       if (!res.ok) { throw new CustomError(ResultCode.UPDATE_FAILED, '更新失败') }
       ctx.body = ResultVO.success()
     } catch (err) {
