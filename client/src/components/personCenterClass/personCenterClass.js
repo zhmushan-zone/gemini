fb4302442c08c892f55f06a0f93e3e51926e429a
shoukailiang <shoukailiang@qq.com>
2018-05-18 16:42:58 +0800

    add class

--- client/src/components/personCenterClass/personCenterClass.js
+++ client/src/components/personCenterClass/personCenterClass.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Tabs } from 'antd'
+import CustomIcon from '@/common/customIcon/customIcon'
 import './personCenterClass.scss'
 const TabPane = Tabs.TabPane
 class PersonCenterClass extends React.Component {
@@ -19,7 +20,40 @@ class PersonCenterClass extends React.Component {
                 </div>
                 <div className="course-list">
                   <ul>
-                    <li className="course-one"></li>
+                    <li className="course-one">
+                      <div className="course-list-img">
+                        <a href="">
+                          <img src={require('@/assets/imgs/test-course.jpg')} alt="" />
+                        </a>
+                      </div>
+                      <div className="course-list-cont">
+                        <h3 className="study-hd">
+                          Storm基础入门
+                          <span className="i-new">更新完毕</span>
+                          {/* 收藏和删除 */}
+                          <div className="share-box">
+                            <div className="show-btn"></div>
+                          </div>
+                        </h3>
+                        <div className="study-points">
+                          <span className="i-left">已学4%</span>
+                          <span className="i-mid">用时11分</span>
+                          <span className="i-right">学习至１－７操作数据库</span>
+                        </div>
+                        <div className="catog-points">
+                          <span className="i-left span-common">
+                            <a>笔记<i>0</i></a>
+                          </span>
+                          <span className="i-mid span-common">
+                            <a>代码<i>0</i></a>
+                          </span>
+                          <span className="i-right span-common">
+                            <a>问答<i>0</i></a>
+                          </span>
+                          <a className="btn-red continute-btn">继续学习</a>
+                        </div>
+                      </div>
+                    </li>
                   </ul>
                 </div>
               </div>
