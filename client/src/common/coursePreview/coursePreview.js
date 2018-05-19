d0d5d4edacfa4e26428985de06265ec4cc17c76e
lizhihao97 <940166841@qq.com>
2018-05-19 23:23:55 +0800

    add lastestCourse component

---
+++ client/src/common/coursePreview/coursePreview.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import CustomIcon from '../customIcon/customIcon'
+import './coursePreview.scss'
+
+class CoursePreview extends React.Component {
+  // 传入评价的值计算应该出现星的数量和类型
+  rateJudgment (rate) {
+    const star = []
+    let i
+    for (i = 1; i <= rate; i ++) {
+      star.push('complete')
+    }
+    if (rate - i + 1> 0.3 && rate - i < 0.8) {
+      star.push('half')
+    } else if (rate - i + 1 >= 0.8) {
+      star.push('complete')
+    }
+
+    return star
+  }
+
+  courseHoverIn (e) {
+    const title = e.currentTarget.querySelector('.course-preview-title')
+    const cover = e.currentTarget.querySelector('.course-preview-cover')
+    title.style.color = '#F01414'
+    cover.style.boxShadow = '0 10px 12px rgba(0, 0, 0, .09)'
+  }
+
+  courseHoverOut (e) {
+    const title = e.currentTarget.querySelector('.course-preview-title')
+    const cover = e.currentTarget.querySelector('.course-preview-cover')
+    title.style.color = '#07111B'
+    cover.style.boxShadow = 'none'
+  }
+
+  render () {
+    return (
+      <div className="course-preview">
+        <a href="#javascript" onMouseEnter={(e) => this.courseHoverIn(e)} onMouseLeave={(e) => this.courseHoverOut(e)}>
+          <img className="course-preview-cover" src={this.props.img} alt={this.props.name}/>
+          <h3 className="course-preview-title">{this.props.name}</h3>
+          <div className="course-preview-data">
+            <span className="course-preview-difficulty-data">{this.props.level}</span>
+            <span className="course-preview-viewer-data">
+              <CustomIcon type="yonghu" size="12" />
+              {this.props.viewerCount}
+            </span>
+            <span className="course-preview-rate-data">
+              {
+                this.rateJudgment(this.props.rate).map((item, index) => {
+                  if (item === 'complete') {
+                    return <CustomIcon type="star1" color="#f29d39" size="14" />
+                  } else {
+                    return <CustomIcon type="star2" color="#f29d39" size="14" />
+                  }
+                })
+              }
+            </span>
+          </div>
+          <div className="course-preview-price">
+              <CustomIcon type="jifen" color="#93999F" />
+              <span>{this.props.price}</span>
+          </div>
+        </a>
+      </div>  
+    )
+  }
+}
+
+export default CoursePreview
--- client/src/components/lastestCourse/lastestCourse.js
+++ client/src/components/lastestCourse/lastestCourse.js
@@ -1,26 +1,25 @@
 import React from 'react'
-
-import './recommend.scss'
 import CoursePreview from '@/common/coursePreview/coursePreview'
 
-class Recommend extends React.Component {
-  
+import './lastestCourse.scss'
+
+class LastestCourse extends React.Component {
   render () {
-    const recommendCourse = recmd.map((item, index) => {
-      const { name, level, viewCount, rate, price, img } = item
+    const lastestCourse = lastest.map((item, index) => {
+      const { name, level, viewerCount, rate, price, img } = item
       return <CoursePreview name={name}
         level={level}
-        viewCount={viewCount}
+        viewerCount={viewerCount}
         rate={rate}
         price={price}
-        img={img}  
-        key={index}/>  
+        img={img}
+        key={index} />
     })
     return (
-      <div className="recommend">
-        <h3>热门课程推荐</h3>
-        <div className="recommend-content">
-          {recommendCourse}
+      <div className="lastest-course">
+        <h3>最新课程</h3>
+        <div className="lastest-content">
+          {lastestCourse}
         </div>
       </div>
     )
@@ -28,7 +27,7 @@ class Recommend extends React.Component {
 }
 
 //用于测试
-const recmd = [
+const lastest = [
   { name: '全网最热Python3入门+进阶 更快上手实际开发', level: '中级', viewerCount: 4236, rate: 4.7, price: 100, img: 'https://img3.mukewang.com/szimg/59b8a486000107fb05400300.jpg' },
   { name: '区块链入门与去中心化应用实战', level: '中级', viewerCount: 4236, rate: 4.8, price: 100, img: 'https://img2.mukewang.com/szimg/5af2a67500016b9905400300.jpg' },
   { name: 'Vue2.5开发去哪儿网App 从零基础入门到实战项目', level: '中级', viewerCount: 4236, rate: 3.0, price: 100, img: 'https://img1.mukewang.com/szimg/5ac2dfe100014a9005400300.jpg' },
@@ -36,4 +35,4 @@ const recmd = [
   { name: 'React Native技术精讲与高质量上线APP开发', level: '中级', viewerCount: 4236, rate: 4.1, price: 100, img: 'https://img4.mukewang.com/szimg/5adfe05e00012ecd05400300.jpg' }
 ]
 
-export default Recommend
+export default LastestCourse
