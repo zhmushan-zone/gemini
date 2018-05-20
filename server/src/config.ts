0eb9fdc5e569e64301131871bc61ecf7a3a6999c
木杉 <zhbnh9988@163.com>
2018-05-20 21:33:44 +0800

    add dir init

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,4 +1,5 @@
 import * as path from 'path'
+import * as fs from 'fs'
 
 export namespace config {
   export const DB_NAME = 'mongodb://localhost/gemini'
@@ -8,4 +9,11 @@ export namespace config {
   export const AVATARS_PATH = path.join(UPLOAD_PATH, 'avatars')
   export const UPLOAD_MAX_FILE_SIZE = '3000 * 1024 * 1024'
   export const PORT = 9999
+
+  export const initDir = () => {
+    if (!fs.existsSync(PUBLIC_PATH)) { fs.mkdir(PUBLIC_PATH, err => { if (err) { console.log(err) } }) }
+    if (!fs.existsSync(UPLOAD_PATH)) { fs.mkdir(UPLOAD_PATH, err => { if (err) { console.log(err) } }) }
+    if (!fs.existsSync(VIDEOS_PATH)) { fs.mkdir(VIDEOS_PATH, err => { if (err) { console.log(err) } }) }
+    if (!fs.existsSync(AVATARS_PATH)) { fs.mkdir(AVATARS_PATH, err => { if (err) { console.log(err) } }) }
+  }
 }
