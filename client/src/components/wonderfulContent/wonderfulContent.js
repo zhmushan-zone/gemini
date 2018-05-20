2663af14f9dbe04108b00d0de69cfaee61c38818
lizhihao97 <940166841@qq.com>
2018-05-20 15:54:27 +0800

    add wonderfulContent component

---
+++ client/src/components/wonderfulContent/wonderfulContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Masonry from 'masonry-layout'
+
+import './wonderfulContent.scss'
+
+class WonderfulContent extends React.Component {
+  render () {
+    return (
+      <div className="wonderfulContent">
+      </div>
+    )
+  }
+}
+
+export default WonderfulContent
