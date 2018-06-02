2b4e9f00fc8c3dc383ac0b9123aff0e4aa22e1a9
lizhihao97 <940166841@qq.com>
2018-06-02 23:42:58 +0800

    perfect onlineStudying menu

--- client/src/containers/onlineStudying/onlineStudying.js
+++ client/src/containers/onlineStudying/onlineStudying.js
@@ -6,24 +6,82 @@ class OnlineStudying extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      direction: 'all',
-      type: 'all'
+      direction: '全部',
+      type: '全部'
     }
   }
   
+  tabChange(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
   render() {
+    const menuDirTabs = direction.map((item, index) => {
+      if (item === this.state.direction) {
+        return <a className="active" href="#javascript" key={index}>{item}</a>
+      } else {
+        return <a className="no-active" 
+                href="#javascript" 
+                key={index}
+                onClick={() => this.tabChange('direction', item)}>
+                {item}
+              </a>
+      }
+    })
+    let menuTypeTabsItems
+    if (this.state.direction === '全部') {
+      const menuTypeTabs = type.join().split(',')
+      menuTypeTabs.unshift('全部')
+      menuTypeTabsItems = menuTypeTabs.map((item, index) => {
+        if (item === this.state.type) {
+          return <a className="active" href="#javascript" key={index}>{item}</a>
+        } else {
+          return <a className="no-active" 
+                  href="#javascript" 
+                  key={index}
+                  onClick={() => this.tabChange('type', item)}>
+                  {item}
+                </a>
+        }
+      })
+    } else {
+      const menuTypeTabs = type[direction.indexOf(this.state.direction) - 1]
+      menuTypeTabs.unshift('全部')
+      menuTypeTabsItems = menuTypeTabs.map((item, index) => {
+        if (item === this.state.type) {
+          return <a className="active" href="#javascript" key={index}>{item}</a>
+        } else {
+          return <a className="no-active" 
+                  href="#javascript" 
+                  key={index}
+                  onClick={() => this.tabChange('type', item)}>
+                  {item}
+                </a>
+        }
+      })
+    }
+
+
     return (
       <div className="online-studying">
         <div className="course-menu">
           <div className="course-menu-direction">
             <span className="course-menu-direction-title">方向:</span>
+            <div className="course-menu-direction-tab">
+              {menuDirTabs}
+            </div>
           </div>
           <div className="course-menu-type">
             <span className="course-menu-type-title">分类:</span>
+            <div className="course-menu-type-tab">
+              {menuTypeTabsItems}
+            </div>
           </div>
         </div>
         <div className="online-studying-courses">
-        
+          
         </div>
       </div>
     )
