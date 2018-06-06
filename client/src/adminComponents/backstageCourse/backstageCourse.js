86cbe165f0ee8468b1f95e14192cfbe7995779ad
lizhihao97 <940166841@qq.com>
2018-06-06 21:04:36 +0800

    add backgroundCourseQuery

--- client/src/adminComponents/backstageCourse/backstageCourse.js
+++ client/src/adminComponents/backstageCourse/backstageCourse.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import BackgroundCourseQuery from '../backgroundCourseQuery/backgroundCourseQuery'
 
 import './backstageCourse.scss'
 
 class BackstageCourse extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      direction: '',
+      type: ''
+    }
+    this.stateChange = this.stateChange.bind(this)
+  }
+  
+  stateChange (value, key) {
+    this.setState({
+      [key]: value
+    })
+  }
   render() {
     return (
       <div className="backstage-course">
-        
+        <BackgroundCourseQuery stateChange={this.stateChange} direction={this.state.direction} />
       </div>
     )
   }
