e70f4d46dee403ced4e3d0c954d6106258cbffd8
shoukailiang <shoukailiang@qq.com>
2018-06-07 19:29:43 +0800

    add video &course-sub-nav

--- client/src/components/video/videoPage.js
+++ client/src/components/video/videoPage.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
-import { DefaultPlayer as Video } from 'react-html5video';
-import 'react-html5video/dist/styles.css';
+import { DefaultPlayer as Video } from 'react-html5video'
+import CustomIcon from '@/common/customIcon/customIcon'
+import VideoHeader from '../videoHeader/videoHeader'
+import VideoContent from '../videoPageContent/videoPageContent'
+import Footer from '../footer/footer'
+import 'react-html5video/dist/styles.css'
+import './videoPage.scss'
 export default class VideoPage extends Component {
   constructor(props) {
     super(props)
@@ -11,17 +16,45 @@ export default class VideoPage extends Component {
   }
   render() {
     return (
-      <div>
-        <Video autoPlay loop muted
-          controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-          poster="http://sourceposter.jpg"
-          onCanPlayThrough={() => {
-            // Do stuff
-          }}>
-          <source src={`/api/videos/${this.props.match.params.id}`} type="video/webm" />
-          {/* <track label="English" kind="subtitles" srcLang="en" src="http://source.vtt" default /> */}
-        </Video>
-      </div>
+      <React.Fragment>
+        <VideoHeader courseName="课程介绍" />
+        <div className="video-page-container">
+          <div className="course-sidebar-layout">
+            <dl>
+              <dd className="openchapter">
+                <CustomIcon type="zhangjiekecheng" size={24} color="white" />
+                <span>章节</span>
+              </dd>
+              <dd>
+                <CustomIcon type="wendaguanli"  size={24} color="white" />
+                <span>问答</span>
+              </dd>
+              <dd>
+                <CustomIcon type="biji"  size={24} color="white" />
+                <span>笔记</span>
+              </dd>
+              <dd>
+                <CustomIcon type="pinglun"  size={24} color="white" />
+                <span>评论</span>
+              </dd>
+            </dl>
+          </div>
+          <Video autoPlay loop muted
+            controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
+            poster="http://sourceposter.jpg"
+            className="video"
+            onCanPlayThrough={() => {
+              // Do stuff
+            }}>
+            <source src={`/api/videos/${this.props.match.params.id}`} type="video/webm" />
+          </Video>
+          <div className="video-panel course-sidebar-layout">
+            <CustomIcon type="icon-arrow-left4" size={24} color="white" />
+          </div>
+        </div>
+        <VideoContent />
+        <Footer />
+      </React.Fragment>
     )
   }
 }
