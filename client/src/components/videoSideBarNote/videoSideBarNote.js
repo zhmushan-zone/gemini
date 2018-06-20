13e080a89d955f20bded2f857e3d68cf82c31be7
shoukailiang <shoukailiang@qq.com>
2018-06-20 10:53:46 +0800

    perfect video siderbar

--- client/src/components/videoSideBarNote/videoSideBarNote.js
+++ client/src/components/videoSideBarNote/videoSideBarNote.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import { Button } from 'antd'
+import SimpleMDE from 'react-simplemde-editor'
 import CustomIcon from '@/common/customIcon/customIcon'
 import './videoSideBarNote.scss'
+import 'simplemde/dist/simplemde.min.css'
 export default class VideoSideBarNote extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      textValue: "",
+    }
+  }
+  handleChange() {
+
+  }
   render() {
     return (
       <div className="video-side-bar-note-container">
-        <h2>note</h2>
-        <span onClick={this.props.closeNoteorQues}>
-          <CustomIcon type='x' color="black" size={24} className="delete" />
-        </span>
+        <div className="top">
+          <h2>写笔记</h2>
+          <span onClick={this.props.closeNoteorQues}>
+            <CustomIcon type='x' color="black" size={24} className="delete" />
+          </span>
+        </div>
+        <SimpleMDE
+          onChange={this.handleChange.bind(this)}
+          value={this.state.textValue}
+        />
+        <p className="button-p">
+          <Button type="primary">提问</Button>
+        </p>
       </div>
     )
   }
--- client/src/components/videoSideBarQuestion/videoSideBarQuestion.js
+++ client/src/components/videoSideBarQuestion/videoSideBarQuestion.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
+import { Input, Button } from 'antd'
+
+import SimpleMDE from 'react-simplemde-editor'
 import CustomIcon from '@/common/customIcon/customIcon'
 import './videoSideBarQuestion.scss'
+import 'simplemde/dist/simplemde.min.css'
 export default class VideoSideBarQuestion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      textValue: "",
+    }
+  }
+  handleChange() {
+
+  }
   render() {
-    
+
     return (
       <div className="side-bar-question-container">
-        <h2>question</h2>
-        <span onClick={this.props.closeNoteorQues}>
-          <CustomIcon type='x' color="black" size={24} className="delete" />
-        </span>
+        <div className="top">
+          <h2>提问题</h2>
+          <span onClick={this.props.closeNoteorQues}>
+            <CustomIcon type='x' color="black" size={24} className="delete" />
+          </span>
+        </div>
+        <Input size="large" placeholder="请输入您的问题" className="question" />
+        <SimpleMDE
+          onChange={this.handleChange.bind(this)}
+          value={this.state.textValue}
+        />
+        <p className="button-p">
+          <Button type="primary">提交</Button>
+        </p>
       </div>
     )
   }
