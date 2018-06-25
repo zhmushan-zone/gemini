3860f5425c9a67cfe224f7d7ba8a155c32bf72ba
木杉 <zhbnh9988@163.com>
2018-06-25 21:07:18 +0800

    Add file apis

---
+++ server/src/file/file.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { FileController } from './file.controller';
+import {CommonModule} from '../common/common.module';
+
+@Module({
+  imports: [
+    CommonModule
+  ],
+  controllers: [FileController]
+})
+export class FileModule {}
