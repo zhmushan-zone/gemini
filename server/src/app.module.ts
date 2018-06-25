a36b26405dcbe7694125a6421f626d8afd15d2ef
木杉 <zhbnh9988@163.com>
2018-06-25 13:09:58 +0800

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,14 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './config';
-import { VideoModule } from './video/video.module';
 import { CourseModule } from './course/course.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config.typeorm),
     UserModule,
-    VideoModule,
     CourseModule
   ],
   controllers: [AppController],
