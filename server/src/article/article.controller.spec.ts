359f4ce85804af7edb9df4e9d836029e96145532
木杉 <zhbnh9988@163.com>
2018-06-26 20:02:07 +0800

    Add article struct

---
+++ server/src/article/article.controller.spec.ts
@@ -0,0 +1,15 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ArticleController } from './article.controller';
+
+describe('Article Controller', () => {
+  let module: TestingModule;
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [ArticleController],
+    }).compile();
+  });
+  it('should be defined', () => {
+    const controller: ArticleController = module.get<ArticleController>(ArticleController);
+    expect(controller).toBeDefined();
+  });
+});
---
+++ server/src/article/article.service.spec.ts
@@ -0,0 +1,15 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ArticleService } from './article.service';
+
+describe('ArticleService', () => {
+  let service: ArticleService;
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ArticleService],
+    }).compile();
+    service = module.get<ArticleService>(ArticleService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
