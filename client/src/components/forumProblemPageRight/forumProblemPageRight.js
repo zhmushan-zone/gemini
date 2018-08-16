1e09ddab464873b7cc2325e505cf2dc3c4e45f7c
lizhihao97 <940166841@qq.com>
2018-08-16 15:16:57 +0800

    add relatedProblem

--- client/src/components/forumProblemPageRight/forumProblemPageRight.js
+++ client/src/components/forumProblemPageRight/forumProblemPageRight.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter ,Link } from 'react-router-dom'
 import ForumProblemPageRelatedType from '../forumProblemPageRelatedType/forumProblemPageRelatedType'
+import ForumProblemPageRelatedProblem from '../forumProblemPageRelatedProblem/forumProblemPageRelatedProblem'
 
 import './forumProblemPageRight.scss'
 
@@ -11,6 +12,7 @@ class ForumProblemPageRight extends Component {
     return (
       <div className="forum-problem-page-right">
         <Link className="forum-problem-page-craete-btn" to={`${this.props.match.url}/create`}>我要提问</Link>
+        <ForumProblemPageRelatedProblem />
         <ForumProblemPageRelatedType 
           type={[0, 1, 2]}
         />
