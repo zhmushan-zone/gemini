23c6bc0766860464de584d3c1e8aa7b3029b5d6f
zhmushan <zhmushan@qq.com>
2018-08-18 20:01:04 +0800

    fix

--- server/src/common/audit-mongo.repository.ts
+++ server/src/common/audit-mongo.repository.ts
@@ -1,31 +1,21 @@
-import { MongoRepository, UpdateResult, DeepPartial, SaveOptions } from 'typeorm';
+import { MongoRepository, UpdateResult, DeepPartial, SaveOptions, ObjectLiteral, ObjectID, FindConditions } from 'typeorm';
 
-export abstract class AuditMongoRepository<Entity> extends MongoRepository<Entity> {
-
+export abstract class AuditMongoRepository<Entity extends ObjectLiteral> extends MongoRepository<Entity> {
   async save<T extends DeepPartial<Entity>>(entity: T, options?: SaveOptions): Promise<T> {
     const createdCol = this.metadata.createDateColumn ? this.metadata.createDateColumn.propertyPath : null;
-    const versionCol = this.metadata.versionColumn ? this.metadata.versionColumn.propertyPath : null;
     if (createdCol) {
       (entity as any)[createdCol] = new Date();
-    }
-    if (versionCol) {
-      (entity as any)[versionCol] = 1;
     }
     return super.save(entity, options);
   }
 
   async update(
-    criteria: string, partialEntity: DeepPartial<Entity>, options?: SaveOptions): Promise<UpdateResult> {
+    criteria: string | string[] | number | number[] | Date | Date[] | ObjectID | ObjectID[] | FindConditions<Entity>,
+    partialEntity: DeepPartial<Entity>, options?: SaveOptions
+  ): Promise<UpdateResult> {
     const updatedCol = this.metadata.updateDateColumn ? this.metadata.updateDateColumn.propertyPath : null;
-    const versionCol = this.metadata.versionColumn ? this.metadata.versionColumn.propertyPath : null;
     if (updatedCol) {
       (partialEntity as any)[updatedCol] = new Date();
-    }
-    if (versionCol) {
-      const found = await super.findOne(criteria);
-      if (found) {
-        (partialEntity as any)[versionCol] = found[versionCol] + 1;
-      }
     }
     return super.update(criteria, partialEntity, options);
   }
