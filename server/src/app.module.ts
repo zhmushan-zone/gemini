f6a720a27a5708ff8a8dcf264d28393238681f72
zhmushan <zhmushan@qq.com>
2018-08-26 22:40:15 +0800

    add report module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,6 +8,7 @@ import { CourseModule } from './course/course.module';
 import { FileModule } from './file/file.module';
 import { ArticleModule } from './article/article.module';
 import { IssueModule } from './issue/issue.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { IssueModule } from './issue/issue.module';
     CourseModule,
     FileModule,
     ArticleModule,
-    IssueModule
+    IssueModule,
+    ReportModule
   ],
   controllers: [AppController],
   providers: [AppService]
