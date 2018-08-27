ebf90ff2d66dfc534733cae616dfc9269a3882b1
shoukailiang <shoukailiang@qq.com>
2018-08-27 15:33:38 +0800

    recommend article

--- client/src/components/opinionRecommend/opinionRecommend.js
+++ client/src/components/opinionRecommend/opinionRecommend.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+import OpinionSideBar from '../opinionSideBar/opinionSideBar'
+import OpinionMainCenter from '../opinionMainCenter/opinionMainCenter'
+import OpinionMainRight from '../opinionMainRight/opinionMainRight'
+import { fetchArticleAll } from '@/redux/actions.js'
+@connect((state) => state, {fetchArticleAll})
 export default class opinionRecommend extends Component {
-  render() {
-    return (
-      <div>
-        推荐
-      </div>
-    )
+  componentWillMount = () => {
+    this.props.fetchArticleAll()
   }
+  
+	render() {
+		return (
+			<div className='opinionMain-container'>
+				<OpinionSideBar />
+				<OpinionMainCenter />
+				<OpinionMainRight />
+			</div>
+		)
+	}
 }
