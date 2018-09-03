25a8141ce8372dd5f43fae67a06329d378f17164
lizhihao97 <940166841@qq.com>
2018-09-03 21:13:20 +0800

    perfect checkCenter

--- client/src/adminComponents/backstageProblemList/backstageProblemList.js
+++ client/src/adminComponents/backstageProblemList/backstageProblemList.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { Input } from 'antd'
 
 import './backstageProblemList.scss'
+
+const Search = Input.Search
 
 class BackstageProblemList extends Component {
   render() {
     return (
       <div className="backstage-problem-list">
-        
+        <div className="backstage-problem-list-top">
+          <Search
+            placeholder="请输入"
+            enterButton="搜索"
+            size="large"
+            onSearch={value => console.log(value)}
+          />
+        </div>
       </div>
     )
   }
