5cbf80d208c6bb3330bf890cab77eb1bd04a0cb2
zhmushan <zhbnh9988@163.com>
2018-09-14 15:38:20 +0800

    ...

--- server/src/course/course.entity.ts
+++ server/src/course/course.entity.ts
@@ -54,6 +54,7 @@ export class Course extends BaseEntity {
     if (!this.sections) this.sections = [];
     if (!this.authorId) this.authorId = '';
     if (!this.commentsId) this.commentsId = [];
+    if (!this.rate) this.rate = 5;
   }
 
   @BeforeUpdate()
