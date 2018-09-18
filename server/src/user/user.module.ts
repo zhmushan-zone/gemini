20f40499bed84c13c19b07619a4db62ce82e887c
zhmushan <zhmushan@qq.com>
2018-09-18 17:40:40 +0800

    fix depend

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -4,11 +4,17 @@ import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from '../common/common.module';
+import { CourseModule } from '../course/course.module';
+import { ArticleModule } from '../article/article.module';
+import { IssueModule } from '../issue/issue.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    CommonModule
+    CommonModule,
+    ArticleModule,
+    IssueModule,
+    CourseModule
   ],
   providers: [
     UserService
