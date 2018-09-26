d494301b5acbcb529525f619d7ae0894a39280bc
shoukailiang <shoukailiang@qq.com>
2018-09-26 08:24:52 +0800

    fix

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -20,78 +20,78 @@ class Nav extends React.Component {
 			searchContent: '',
 			shoppingCartCount: 0,
 			isHaveUnRead: false,
-			isMessageCenter: false
+			isMessageCenter: false,
 		}
 		this.stateChange = this.stateChange.bind(this)
 	}
 
 	stateChange(key, value) {
 		this.setState({
-			[key]: value
+			[key]: value,
 		})
 	}
 
 	async componentDidMount() {
 		const { pathname } = this.props.location
+
 		if (pathname === '/onlineStudying' || pathname.split('/')[1] === 'class') {
 			this.setState({
-				selected: 1
+				selected: 1,
 			})
 		} else if (pathname === '/forum') {
 			this.setState({
-				selected: 2
+				selected: 2,
 			})
-		} else if (pathname === '/opinion') {
+		} else if (
+			pathname === '/opinion' ||
+			pathname === '/opinion/focus' ||
+			pathname.split('/')[1] === 'article' ||
+			pathname.split('/')[1] === 'excellentSeven' ||
+			pathname.split('/')[1] === 'excellentThirty'
+		) {
 			this.setState({
-				selected: 3
-			})
-		} else if (pathname === '/opinion/focus') {
-			this.setState({
-				selected: 3
-			})
-		} else if (pathname.split('/')[1] === 'article') {
-			this.setState({
-				selected: 3
-			})
-		} else if (pathname.split('/')[1] === 'excellentSeven') {
-			this.setState({
-				selected: 3
-			})
-		} else if (pathname.split('/')[1] === 'excellentThirty') {
-			this.setState({
-				selected: 3
-			})
-		} else if (pathname.split('/')[1] === 'messageCenter') {
-			this.setState({
-				selected: 1
+				selected: 3,
 			})
 		}
 		await this.props.fetchMessage()
 		this.props.msg.map((item) => {
 			if (item.isRead === false) {
 				return this.setState({
-					isHaveUnRead: true
+					isHaveUnRead: true,
 				})
 			}
 		})
 	}
 
 	componentWillReceiveProps(nextProps) {
-		// if(nextProps.location.pathname.split('/')[1]==='messageCenter'){
-		//   this.setState({
-		//     isMessageCenter:true
-		//   })
-		// }
+		const { pathname } = nextProps.location
+		if (pathname === '/home') {
+			this.setState({
+				selected: 0,
+			})
+		} else if (pathname === '/onlineStudying') {
+			this.setState({
+				selected: 1,
+			})
+		}else if (pathname === '/forum') {
+			this.setState({
+				selected: 2,
+			})
+		}else if (pathname === '/opinion') {
+			this.setState({
+				selected: 3,
+			})
+		}
 		if (nextProps.msg !== this.props.msg) {
 			nextProps.msg.map((item) => {
 				if (item.isRead === false) {
 					return this.setState({
-						isHaveUnRead: true
+						isHaveUnRead: true,
 					})
 				}
 			})
 			return this.setState({
-				isHaveUnRead: false
+				isHaveUnRead: false,
 			})
 		}
 	}
@@ -102,7 +102,7 @@ class Nav extends React.Component {
 			const target = e.target
 
 			this.setState({
-				selected: parseInt(target.getAttribute('data-identify'), 10)
+				selected: parseInt(target.getAttribute('data-identify'), 10),
 			})
 
 			const dataIdentify = target.getAttribute('data-identify')
@@ -120,7 +120,7 @@ class Nav extends React.Component {
 		var is = this.props.location.pathname.split('/')[1] === 'personCenter'
 		let isMessageCenter = this.props.location.pathname.split('/')[1] === 'messageCenter'
 		const personCenterNavBgColor = classnames({
-			personCenterNavBgColor: is
+			personCenterNavBgColor: is,
 		})
 		const navList = [ '首页', '在线学习', '答疑', '看法' ]
 		const navRoute = [ '/home', '/onlineStudying', '/forum', '/opinion' ]
@@ -130,7 +130,7 @@ class Nav extends React.Component {
 					<li key={index}>
 						<NavLink
 							className={is ? '' : 'selected'}
-							style={is ? { color: 'white' } : isMessageCenter ? { color: '#4d555d' } : ''}
+							style={is ? { color: 'white' } : isMessageCenter ? { color: '#4d555d' } : {}}
 							to={navRoute[index]}
 							onClick={(e) => this.navSectionClick(e)}
 							data-identify={index}
@@ -144,7 +144,7 @@ class Nav extends React.Component {
 					<li key={index}>
 						<NavLink
 							to={navRoute[index]}
-							style={is ? { color: 'white' } : null}
+							style={is ? { color: 'white' } : {}}
 							onClick={(e) => this.navSectionClick(e)}
 							data-identify={index}
 						>
@@ -155,12 +155,12 @@ class Nav extends React.Component {
 			}
 		})
 		return (
-			<nav style={is ? { boxShadow: 'none' } : null} className="allNav">
-				<div className="nav-left">
-					<div className="nav-logo">
-						<img src={require(`@/assets/imgs/logo.png`)} alt="" />
+			<nav style={is ? { boxShadow: 'none' } : null} className='allNav'>
+				<div className='nav-left'>
+					<div className='nav-logo'>
+						<img src={require(`@/assets/imgs/logo.png`)} alt='' />
 					</div>
-					<div className="nav-sections">
+					<div className='nav-sections'>
 						<ul>{navSections}</ul>
 						{is || isMessageCenter ? null : (
 							<span
@@ -170,36 +170,36 @@ class Nav extends React.Component {
 						)}
 					</div>
 				</div>
-				<div className="nav-operation">
-					<div className="nav-search-wrapper">
-						<div className="nav-search">
+				<div className='nav-operation'>
+					<div className='nav-search-wrapper'>
+						<div className='nav-search'>
 							<input
-								type="text"
+								type='text'
 								style={is ? { color: 'white' } : null}
 								onChange={(e) => this.setState({ searchContent: e.target.value })}
 							/>
 							<span>
 								<a onClick={() => this.search()}>
-									<Icon type="search" theme="outlined" />
+									<Icon type='search' theme='outlined' />
 								</a>
 							</span>
 						</div>
 					</div>
 					{Cookies.get('_token') ? (
 						<React.Fragment>
-							<div className="nav-shopping-cart-wrapper" style={{ paddingTop: 12 }}>
-								<Link to={`/shoppingCart`} className="nav-shopping-cart">
-									<Icon type="shopping-cart" theme="outlined" />
-									<span style={{ marginLeft: 10 }}>购物车</span>
+							<div className='nav-shopping-cart-wrapper' style={{ paddingTop: 12 }}>
+								<Link to={`/shoppingCart`} className='nav-shopping-cart'>
+									<Icon type='shopping-cart' theme='outlined' />
+									<span style={{ marginLeft: 10 + 'px' }}>购物车</span>
 									{this.state.shoppingCartCount ? (
-										<span className="nav-shopping-cart-count">{this.state.shoppingCartCount}</span>
+										<span className='nav-shopping-cart-count'>{this.state.shoppingCartCount}</span>
 									) : null}
 								</Link>
 							</div>
-							<div className="nav-message-center-wrapper">
-								<Link to={'/messageCenter'} className="nav-message-center">
-									<Icon type="bell" theme="filled" />
-									{this.state.isHaveUnRead ? <span className="new-message-alert" /> : null}
+							<div className='nav-message-center-wrapper'>
+								<Link to={'/messageCenter'} className='nav-message-center'>
+									<Icon type='bell' theme='filled' />
+									{this.state.isHaveUnRead ? <span className='new-message-alert' /> : null}
 								</Link>
 							</div>
 						</React.Fragment>
